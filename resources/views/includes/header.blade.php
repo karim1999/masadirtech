194e197c044dfba7215d71765ac9b2addcf15bf3
Karim Abdelgwad <karim.elbadry2@gmail.com>
2020-08-07 21:51:41 +0200

    analytics

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -31,6 +31,15 @@
     </title>
     {!!$api['site_profile']->google_analytics!!}
 
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-174669370-2"></script>
+      <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-174669370-2');
+      </script>
 
 
 
